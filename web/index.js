999dba29f60e757860f07acb0cb1845c7e116632
Daniele Santini <danysan95@gmail.com>
2021-08-22 16:22:41 +0200

    Color legend

--- web/index.js
+++ web/index.js
@@ -1,99 +1,122 @@
-const defaultBackgroundStyle = 'mapbox://styles/mapbox/streets-v11',
+const defaultBackgroundStyle = 'mapbox://styles/mapbox/light-v10',
     backgroundStyles = {
         'Streets': 'mapbox://styles/mapbox/streets-v11',
         'Light': 'mapbox://styles/mapbox/light-v10',
         'Dark': 'mapbox://styles/mapbox/dark-v10',
         'Satellite': 'mapbox://styles/mapbox/satellite-v9',
     },
-    defaultColorScheme = '#3bb2d0',
+    defaultColorScheme = 'blue',
     colorSchemes = {
-        'Blue': '#3bb2d0',
-        'By gender': [
-            // https://www.wikidata.org/wiki/Property:P21
-            // https://meyerweb.com/eric/tools/color-blend/#3BB2D0:E55E5E:3:hex
-            'match', ['get', 'genderID', ['at', 0, ['get', 'etymologies']]],
-            'http://www.wikidata.org/entity/Q6581072', '#e55e5e', // female
-            'http://www.wikidata.org/entity/Q1052281', '#BB737B', // transgender female
-            'http://www.wikidata.org/entity/Q1097630', '#908897', // intersex
-            'http://www.wikidata.org/entity/Q2449503', '#669DB4', // transgender male
-            'http://www.wikidata.org/entity/Q6581097', '#3bb2d0', // male
-            '#223b53' // other
-        ],
-        'By type': [
-            'match', ['get', 'instanceID', ['at', 0, ['get', 'etymologies']]],
-            // People
-            'http://www.wikidata.org/entity/Q5', '#3bb2d0', // human
-            'http://www.wikidata.org/entity/Q21070568', '#3bb2d0', // human who may be fictional
-            // Buildings
-            'http://www.wikidata.org/entity/Q23413', '#fbb03b', // castle
-            'http://www.wikidata.org/entity/Q751876', '#fbb03b', // château
-            'http://www.wikidata.org/entity/Q684740', '#fbb03b', // real property
-            'http://www.wikidata.org/entity/Q811979', '#fbb03b', // architectural structure
-            'http://www.wikidata.org/entity/Q1516079', '#fbb03b', // cultural heritage ensemble
-            'http://www.wikidata.org/entity/Q33506', '#fbb03b', // museum
-            'http://www.wikidata.org/entity/Q16970', '#fbb03b', // church
-            'http://www.wikidata.org/entity/Q233324', '#fbb03b', // seminary
-            'http://www.wikidata.org/entity/Q160742', '#fbb03b', // abbey
-            'http://www.wikidata.org/entity/Q817056', '#fbb03b', // benedictine abbey
-            'http://www.wikidata.org/entity/Q163687', '#fbb03b', // basilica
-            'http://www.wikidata.org/entity/Q120560', '#fbb03b', // minor basilica
-            'http://www.wikidata.org/entity/Q44613', '#fbb03b', // monastery
-            'http://www.wikidata.org/entity/Q1564373', '#fbb03b', // mission complex
-            'http://www.wikidata.org/entity/Q179700', '#fbb03b', // statue
-            'http://www.wikidata.org/entity/Q1779653', '#fbb03b', // colossal statue
-            // Historic events
-            'http://www.wikidata.org/entity/Q178561', '#e55e5e', // battle
-            'http://www.wikidata.org/entity/Q188055', '#e55e5e', // siege
-            'http://www.wikidata.org/entity/Q3199915', '#e55e5e', // massacre
-            'http://www.wikidata.org/entity/Q750215', '#e55e5e', // mass murder
-            'http://www.wikidata.org/entity/Q891854', '#e55e5e', // bomb attack
-            'http://www.wikidata.org/entity/Q898712', '#e55e5e', // aircraft hijacking
-            'http://www.wikidata.org/entity/Q217327', '#e55e5e', // suicide attack
-            'http://www.wikidata.org/entity/Q2223653', '#e55e5e', // terrorist attack
-            'http://www.wikidata.org/entity/Q175331', '#e55e5e', // Demonstration
-            // Cities
-            'http://www.wikidata.org/entity/Q515', '#fed976', // city
-            'http://www.wikidata.org/entity/Q1549591', '#fed976', // big city
-            'http://www.wikidata.org/entity/Q702492', '#fed976', // urban area
-            'http://www.wikidata.org/entity/Q956214', '#fed976', // chef-lieu
-            'http://www.wikidata.org/entity/Q1637706', '#fed976', // million city
-            'http://www.wikidata.org/entity/Q747074', '#fed976', // comune of Italy
-            'http://www.wikidata.org/entity/Q42744322', '#fed976', // urban municipality of Germany
-            'http://www.wikidata.org/entity/Q15105893', '#fed976', // town in Croatia
-            'http://www.wikidata.org/entity/Q2264924', '#fed976', // port settlement
-            'http://www.wikidata.org/entity/Q15661340', '#fed976', // ancient city
-            'http://www.wikidata.org/entity/Q902814', '#fed976', // border town
-            'http://www.wikidata.org/entity/Q5119', '#fed976', // capital
-            'http://www.wikidata.org/entity/Q2202509', '#fed976', // roman city
-            'http://www.wikidata.org/entity/Q3957', '#fed976', // town
-            'http://www.wikidata.org/entity/Q486972', '#fed976', // human settlement
-            'http://www.wikidata.org/entity/Q4946461', '#fed976', // spa-town
-            'http://www.wikidata.org/entity/Q15135589', '#fed976', // religious site
-            'http://www.wikidata.org/entity/Q15303838', '#fed976', // municipality seat
-            'http://www.wikidata.org/entity/Q123705', '#fed976', // neighborhood
-            'http://www.wikidata.org/entity/Q7315416', '#fed976', // residence park
-            // Locations
-            'http://www.wikidata.org/entity/Q1414991', '#348C31', // area
-            'http://www.wikidata.org/entity/Q1620908', '#348C31', // historical region
-            'http://www.wikidata.org/entity/Q35657', '#348C31', // U.S. state
-            'http://www.wikidata.org/entity/Q23442', '#348C31', // island
-            'http://www.wikidata.org/entity/Q8502', '#348C31', // mountain
-            'http://www.wikidata.org/entity/Q46831', '#348C31', // mountain range
-            'http://www.wikidata.org/entity/Q3777462', '#348C31', // alpine group
-            'http://www.wikidata.org/entity/Q54050', '#348C31', // hill
-            'http://www.wikidata.org/entity/Q8072', '#348C31', // volcano
-            'http://www.wikidata.org/entity/Q169358', '#348C31', // stratovolcano
-            'http://www.wikidata.org/entity/Q4421', '#348C31', // forest
-            'http://www.wikidata.org/entity/Q570116', '#348C31', // tourist attraction
-            'http://www.wikidata.org/entity/Q22698', '#348C31', // park
-            // Other
-            '#223b53'
-        ],
-        'Black': '#223b53',
-        'Red': '#e55e5e',
-        'Orange': '#fbb03b',
+        blue: { text: 'Blue', color: '#3bb2d0', legend: null },
+        gender: {
+            text: 'By gender',
+            color: [
+                // https://www.wikidata.org/wiki/Property:P21
+                // https://meyerweb.com/eric/tools/color-blend/#3BB2D0:E55E5E:3:hex
+                'match', ['get', 'genderID', ['at', 0, ['get', 'etymologies']]],
+                'http://www.wikidata.org/entity/Q6581072', '#e55e5e', // female
+                'http://www.wikidata.org/entity/Q1052281', '#BB737B', // transgender female
+                'http://www.wikidata.org/entity/Q1097630', '#908897', // intersex
+                'http://www.wikidata.org/entity/Q2449503', '#669DB4', // transgender male
+                'http://www.wikidata.org/entity/Q6581097', '#3bb2d0', // male
+                '#223b53' // other
+            ],
+            legend: [
+                ['#e55e5e', 'Female'],
+                ['#BB737B', 'Transgender female'],
+                ['#908897', 'Intersex'],
+                ['#669DB4', 'Transgender male'],
+                ['#3bb2d0', 'Male'],
+                ['#223b53', 'Other']
+            ]
+        },
+        type: {
+            text: 'By type',
+            color: [
+                'match', ['get', 'instanceID', ['at', 0, ['get', 'etymologies']]],
+                // People
+                'http://www.wikidata.org/entity/Q5', '#3bb2d0', // human
+                'http://www.wikidata.org/entity/Q21070568', '#3bb2d0', // human who may be fictional
+                // Buildings
+                'http://www.wikidata.org/entity/Q23413', '#fbb03b', // castle
+                'http://www.wikidata.org/entity/Q751876', '#fbb03b', // château
+                'http://www.wikidata.org/entity/Q684740', '#fbb03b', // real property
+                'http://www.wikidata.org/entity/Q811979', '#fbb03b', // architectural structure
+                'http://www.wikidata.org/entity/Q1516079', '#fbb03b', // cultural heritage ensemble
+                'http://www.wikidata.org/entity/Q33506', '#fbb03b', // museum
+                'http://www.wikidata.org/entity/Q16970', '#fbb03b', // church
+                'http://www.wikidata.org/entity/Q233324', '#fbb03b', // seminary
+                'http://www.wikidata.org/entity/Q160742', '#fbb03b', // abbey
+                'http://www.wikidata.org/entity/Q817056', '#fbb03b', // benedictine abbey
+                'http://www.wikidata.org/entity/Q163687', '#fbb03b', // basilica
+                'http://www.wikidata.org/entity/Q120560', '#fbb03b', // minor basilica
+                'http://www.wikidata.org/entity/Q44613', '#fbb03b', // monastery
+                'http://www.wikidata.org/entity/Q1564373', '#fbb03b', // mission complex
+                'http://www.wikidata.org/entity/Q179700', '#fbb03b', // statue
+                'http://www.wikidata.org/entity/Q1779653', '#fbb03b', // colossal statue
+                'http://www.wikidata.org/entity/Q3918', '#fbb03b', // university
+                // Historic events
+                'http://www.wikidata.org/entity/Q178561', '#e55e5e', // battle
+                'http://www.wikidata.org/entity/Q188055', '#e55e5e', // siege
+                'http://www.wikidata.org/entity/Q3199915', '#e55e5e', // massacre
+                'http://www.wikidata.org/entity/Q750215', '#e55e5e', // mass murder
+                'http://www.wikidata.org/entity/Q891854', '#e55e5e', // bomb attack
+                'http://www.wikidata.org/entity/Q898712', '#e55e5e', // aircraft hijacking
+                'http://www.wikidata.org/entity/Q217327', '#e55e5e', // suicide attack
+                'http://www.wikidata.org/entity/Q2223653', '#e55e5e', // terrorist attack
+                'http://www.wikidata.org/entity/Q175331', '#e55e5e', // Demonstration
+                // Cities
+                'http://www.wikidata.org/entity/Q515', '#fed976', // city
+                'http://www.wikidata.org/entity/Q1549591', '#fed976', // big city
+                'http://www.wikidata.org/entity/Q702492', '#fed976', // urban area
+                'http://www.wikidata.org/entity/Q956214', '#fed976', // chef-lieu
+                'http://www.wikidata.org/entity/Q1637706', '#fed976', // million city
+                'http://www.wikidata.org/entity/Q747074', '#fed976', // comune of Italy
+                'http://www.wikidata.org/entity/Q42744322', '#fed976', // urban municipality of Germany
+                'http://www.wikidata.org/entity/Q15105893', '#fed976', // town in Croatia
+                'http://www.wikidata.org/entity/Q2264924', '#fed976', // port settlement
+                'http://www.wikidata.org/entity/Q15661340', '#fed976', // ancient city
+                'http://www.wikidata.org/entity/Q902814', '#fed976', // border town
+                'http://www.wikidata.org/entity/Q5119', '#fed976', // capital
+                'http://www.wikidata.org/entity/Q2202509', '#fed976', // roman city
+                'http://www.wikidata.org/entity/Q3957', '#fed976', // town
+                'http://www.wikidata.org/entity/Q486972', '#fed976', // human settlement
+                'http://www.wikidata.org/entity/Q4946461', '#fed976', // spa-town
+                'http://www.wikidata.org/entity/Q15135589', '#fed976', // religious site
+                'http://www.wikidata.org/entity/Q15303838', '#fed976', // municipality seat
+                'http://www.wikidata.org/entity/Q123705', '#fed976', // neighborhood
+                'http://www.wikidata.org/entity/Q7315416', '#fed976', // residence park
+                // Locations
+                'http://www.wikidata.org/entity/Q1414991', '#348C31', // area
+                'http://www.wikidata.org/entity/Q1620908', '#348C31', // historical region
+                'http://www.wikidata.org/entity/Q35657', '#348C31', // U.S. state
+                'http://www.wikidata.org/entity/Q23442', '#348C31', // island
+                'http://www.wikidata.org/entity/Q8502', '#348C31', // mountain
+                'http://www.wikidata.org/entity/Q46831', '#348C31', // mountain range
+                'http://www.wikidata.org/entity/Q3777462', '#348C31', // alpine group
+                'http://www.wikidata.org/entity/Q54050', '#348C31', // hill
+                'http://www.wikidata.org/entity/Q8072', '#348C31', // volcano
+                'http://www.wikidata.org/entity/Q169358', '#348C31', // stratovolcano
+                'http://www.wikidata.org/entity/Q4421', '#348C31', // forest
+                'http://www.wikidata.org/entity/Q570116', '#348C31', // tourist attraction
+                'http://www.wikidata.org/entity/Q22698', '#348C31', // park
+                // Other
+                '#223b53'
+            ],
+            legend: [
+                ['#3bb2d0', "Human"],
+                ['#fbb03b', 'Buildings'],
+                ['#e55e5e', 'Historic events'],
+                ['#fed976', 'Cities'],
+                ['#348C31', 'Locations'],
+                ['#223b53', 'Other']
+            ]
+        },
+        black: { text: 'Black', color: '#223b53', legend: null },
+        red: { text: 'Red', color: '#e55e5e', legend: null },
+        orange: { text: 'Orange', color: '#fbb03b', legend: null },
     };
-console.info({
+console.info("init", {
     thresholdZoomLevel,
     minZoomLevel,
     defaultBackgroundStyle,
@@ -138,7 +161,7 @@ class BackgroundStyleControl {
         td2.appendChild(ctrlBtn);
 
         this._ctrlDropDown = document.createElement('select');
-        this._ctrlDropDown.className = 'hiddenDropDown';
+        this._ctrlDropDown.className = 'hiddenElement';
         this._ctrlDropDown.title = 'Background style';
         this._ctrlDropDown.onchange = this.dropDownClickHandler.bind(this);
         td1.appendChild(this._ctrlDropDown);
@@ -174,7 +197,7 @@ class BackgroundStyleControl {
 
         this._map.setStyle(backgroundStyle);
 
-        this._ctrlDropDown.className = 'hiddenDropDown';
+        this._ctrlDropDown.className = 'hiddenElement';
         //updateDataSource(event);
     }
 
@@ -188,6 +211,8 @@ class BackgroundStyleControl {
  * @see https://docs.mapbox.com/mapbox-gl-js/example/data-driven-circle-colors/
  * @see https://docs.mapbox.com/mapbox-gl-js/example/color-switcher/
  * @see https://docs.mapbox.com/mapbox-gl-js/api/map/#map#setpaintproperty
+ * @see https://docs.mapbox.com/help/tutorials/choropleth-studio-gl-pt-1/
+ * @see https://docs.mapbox.com/help/tutorials/choropleth-studio-gl-pt-2/
  **/
 class EtymologyColorControl {
 
@@ -199,6 +224,10 @@ class EtymologyColorControl {
 
         const table = document.createElement('table');
         this._container.appendChild(table);
+
+        this._legend = document.createElement('table');
+        this._legend.className = 'legend';
+        this._container.appendChild(this._legend);
 
         const tr = document.createElement('tr');
         table.appendChild(tr);
@@ -217,16 +246,15 @@ class EtymologyColorControl {
         td2.appendChild(ctrlBtn);
 
         this._ctrlDropDown = document.createElement('select');
-        this._ctrlDropDown.className = 'hiddenDropDown';
+        this._ctrlDropDown.className = 'hiddenElement';
         this._ctrlDropDown.title = 'Color scheme';
         this._ctrlDropDown.onchange = this.dropDownClickHandler.bind(this);
         td1.appendChild(this._ctrlDropDown);
 
-        for (const [text, value] of Object.entries(colorSchemes)) {
+        for (const [value, scheme] of Object.entries(colorSchemes)) {
             const option = document.createElement('option');
-            option.innerText = text;
-            //option.value = value;
-            option.value = text;
+            option.innerText = scheme.text;
+            option.value = value;
             if (value === defaultColorScheme) {
                 option.selected = true;
             }
@@ -248,7 +276,7 @@ class EtymologyColorControl {
 
     dropDownClickHandler(event) {
         //const colorScheme = event.target.value;
-        const colorScheme = colorSchemes[event.target.value];
+        const colorScheme = colorSchemes[event.target.value].color;
         console.info("EtymologyColorControl dropDown click", { event, colorScheme });
 
         [
@@ -261,7 +289,28 @@ class EtymologyColorControl {
             }
         });
 
-        this._ctrlDropDown.className = 'hiddenDropDown';
+        const legend = colorSchemes[event.target.value].legend;
+        if (legend) {
+            this._legend.innerHTML = '';
+            legend.forEach(row => {
+                const tr = document.createElement('tr'),
+                    value = document.createElement('td'),
+                    valueColor = document.createElement('span'),
+                    text = document.createElement('td');
+                tr.className = 'legend-row';
+                valueColor.className = 'legend-key';
+                valueColor.style.backgroundColor = row[0];
+                text.innerText = row[1];
+                value.appendChild(valueColor);
+                tr.appendChild(value);
+                tr.appendChild(text);
+                this._legend.appendChild(tr);
+            });
+            this._legend.className = 'legend';
+        } else {
+            this._ctrlDropDown.className = 'hiddenElement';
+            this._legend.className = 'legend hiddenElement';
+        }
         //updateDataSource(event);
     }
 
@@ -429,7 +478,7 @@ function prepareWikidataLayers(wikidata_url) {
         'paint': {
             'circle-radius': 8,
             'circle-stroke-width': 2,
-            'circle-color': defaultColorScheme,
+            'circle-color': colorSchemes[defaultColorScheme].color,
             'circle-stroke-color': 'white'
         }
     });*/
@@ -441,7 +490,7 @@ function prepareWikidataLayers(wikidata_url) {
         "filter": ["==", ["geometry-type"], "LineString"],
         "minzoom": thresholdZoomLevel,
         'paint': {
-            'line-color': defaultColorScheme,
+            'line-color': colorSchemes[defaultColorScheme].color,
             'line-opacity': 0.5,
             'line-width': 7
         }
@@ -454,7 +503,7 @@ function prepareWikidataLayers(wikidata_url) {
         "filter": ["==", ["geometry-type"], "Polygon"],
         "minzoom": thresholdZoomLevel,
         'paint': {
-            'fill-color': defaultColorScheme,
+            'fill-color': colorSchemes[defaultColorScheme].color,
             'fill-opacity': 0.5
         }
     });
@@ -486,7 +535,9 @@ function prepareWikidataLayers(wikidata_url) {
         map.on('mouseleave', layerID, () => map.getCanvas().style.cursor = '');
     });
 
-    setTimeout(() => map.addControl(new EtymologyColorControl()), 1000);
+    if (document.getElementsByClassName("etymology-color-ctrl").length == 0) {
+        setTimeout(() => map.addControl(new EtymologyColorControl()), 1000);
+    }
 }
 
 /**
