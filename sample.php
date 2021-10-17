3f0e1d4e3fdfc820deac4c1b7d32b3806ce5923e
Daniele Santini <danysan95@gmail.com>
2021-10-17 21:00:42 +0200

    Sparql syntax improvement tests

--- sample.php
+++ sample.php
@@ -6,10 +6,9 @@ if (empty($argv[1])) {
 $inputString = $argv[1];
 
 if (empty($argv[2]) || !is_numeric($argv[2])) {
-    echo "Please provide a number as the first argument.\n";
+    echo "Please provide a number as the second argument.\n";
     exit(2);
 }
-$inputNumber = (int)$argv[2];
 
 if (strtolower($inputString) == "sophox") {
     /*
@@ -52,58 +51,67 @@ if (strtolower($inputString) == "sophox") {
     exit(3);
 }
 
-$fileName = "samples/$folder/$inputNumber.$inputExtension";
-if (!file_exists($fileName)) {
-    echo "File $fileName does not exist.\n";
-    exit(4);
-}
-$query = file_get_contents($fileName);
-if ($inputExtension == "rq") {
-    $query = preg_replace('/(^\s+)|(\s*#[\/\s\w\(\),\']+$)/m', "", $query);
-}
+$ret = 0;
+for ($i = 2; $i < $argc; $i++) {
+    if (empty($argv[$i]) || !is_numeric($argv[$i])) {
+        echo "Invalid number argument.\n";
+        exit(2);
+    }
+    $inputNumber = (int)$argv[$i];
 
-//$queryString = http_build_query(["query"=>$query]);
-//$url = "$baseURL?$queryString";
-$url = $baseURL . urlencode($query);
-echo "Querying $url\n";
-
-$curl = curl_init();
-curl_setopt_array($curl, [
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_USERAGENT => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
-    CURLOPT_SSL_VERIFYHOST => 0,
-    CURLOPT_SSL_VERIFYPEER => 0
-]);
-
-$responseBody = curl_exec($curl);
-
-if ($responseBody === false) {
-    echo "Call failure.\n";
-
-    $curlError = curl_error($curl);
-    echo "Error: $curlError\n";
-
-    $ret = 5;
-} else {
-    $httpCode = curl_getinfo($curl, CURLINFO_RESPONSE_CODE);
-    echo "HTTP code: $httpCode\n";
-
-    if ($httpCode == 200) {
-        echo "Call successful.\n";
-        $outFileName = "samples/$folder/$inputNumber-output.$outputExtension";
-        file_put_contents($outFileName, $responseBody);
-        echo "Output written to $outFileName\n";
-        $ret = 0;
-    } else {
-        echo "Call failed.\n";
-        echo "Response body:\n$responseBody\n";
-        $ret = 6;
+    $fileName = "samples/$folder/$inputNumber.$inputExtension";
+    if (!file_exists($fileName)) {
+        echo "File $fileName does not exist.\n";
+        exit(4);
+    }
+    $query = file_get_contents($fileName);
+    if ($inputExtension == "rq") {
+        $query = preg_replace('/(^\s+)|(\s*#[\/\s\w\(\),\']+$)/m', "", $query);
     }
 
-    $timeTaken = curl_getinfo($curl, CURLINFO_TOTAL_TIME);
-    echo "Time taken: $timeTaken s\n";
-}
+    //$queryString = http_build_query(["query"=>$query]);
+    //$url = "$baseURL?$queryString";
+    $url = $baseURL . urlencode($query);
+    //echo "Querying $url\n";
 
-curl_close($curl);
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_USERAGENT => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
+        CURLOPT_SSL_VERIFYHOST => 0,
+        CURLOPT_SSL_VERIFYPEER => 0
+    ]);
+
+    $responseBody = curl_exec($curl);
+
+    if ($responseBody === false) {
+        echo "Call failure.\n";
+
+        $curlError = curl_error($curl);
+        echo "Error: $curlError\n";
+
+        $ret = 5;
+    } else {
+        $httpCode = (int)curl_getinfo($curl, CURLINFO_RESPONSE_CODE);
+        echo "HTTP code: $httpCode\n";
+
+        if ($httpCode == 200) {
+            echo "Call successful.\n";
+            $outFileName = "samples/$folder/$inputNumber-output.$outputExtension";
+            file_put_contents($outFileName, $responseBody);
+            echo "Output written to $outFileName\n";
+            $ret = 0;
+        } else {
+            echo "Call failed.\n";
+            echo "Response body:\n$responseBody\n";
+            $ret = 6;
+        }
+
+        $timeTaken = curl_getinfo($curl, CURLINFO_TOTAL_TIME);
+        echo "Time taken: $timeTaken s\n";
+    }
+
+    curl_close($curl);
+}
 exit($ret);
