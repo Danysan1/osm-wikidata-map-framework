5ea5e129073998343188cbe0a639107c0bf45ba7
Daniele Santini <danysan95@gmail.com>
2021-11-07 18:09:10 +0100

    Date Precision + improve gitignore

--- sample.php
+++ sample.php
@@ -13,64 +13,86 @@ function getOverpassEndpoint(): string
 }
 
 if (empty($argv[1])) {
-    echo "Please provide a string as the first argument." . PHP_EOL;
+    echo "Please provide the query type as the first argument." . PHP_EOL;
     exit(1);
 }
 $inputString = $argv[1];
 
-if (empty($argv[2]) || !is_numeric($argv[2])) {
-    echo "Please provide a number as the second argument." . PHP_EOL;
+if (empty($argv[2])) {
+    echo "Please provide the query name as the second argument." . PHP_EOL;
     exit(2);
 }
 
-if (strtolower($inputString) == "sophox") {
-    /*
-    https://wiki.openstreetmap.org/wiki/Sophox
-    https://sophox.org/
-    */
-    $baseURL = 'https://sophox.org/sparql?query=';
-    $folder = "sophox";
-    $inputExtension = "rq";
-    $outputExtension = "xml";
-} elseif (strtolower($inputString) == "wikidata") {
-    /*
-    https://www.mediawiki.org/wiki/Wikidata_Query_Service/User_Manual#SPARQL_endpoint
-    */
-    $baseURL = 'https://query.wikidata.org/sparql?query=';
-    $folder = "wikidata";
-    $inputExtension = "rq";
-    $outputExtension = "xml";
-} elseif (strtolower($inputString) == "overpassql") {
-    /* 
-    https://wiki.openstreetmap.org/wiki/Overpass_API#Public_Overpass_API_instances
-    https://wiki.openstreetmap.org/wiki/Overpass_API/Language_Guide
-    https://wiki.openstreetmap.org/wiki/Overpass_turbo/Extended_Overpass_Turbo_Queries
-    */
-    $baseURL = getOverpassEndpoint()."?data=";
-    $folder = "overpassql";
-    $inputExtension = "overpass";
-    $outputExtension = "json";
-} elseif (strtolower($inputString) == "overpassxml") {
-    /*
-    https://wiki.openstreetmap.org/wiki/Overpass_API#Public_Overpass_API_instances
-    https://wiki.openstreetmap.org/wiki/Overpass_API/Language_Guide
-    */
-    $baseURL = getOverpassEndpoint()."?data=";
-    $folder = "overpassxml";
-    $inputExtension = "xml";
-    $outputExtension = "json";
-} else {
-    echo "Please provide a VALID string as the first argument.".PHP_EOL;
-    exit(3);
+switch (strtolower($inputString)) {
+    case "sophox":
+        /*
+        https://wiki.openstreetmap.org/wiki/Sophox
+        https://sophox.org/
+        */
+        $baseURL = 'https://sophox.org/sparql?query=';
+        $folder = "sophox";
+        $inputExtension = "rq";
+        $outputExtension = "xml";
+        break;
+
+    case "wikidata":
+    case "wikidata-xml":
+    case "wdx":
+        /*
+        https://www.mediawiki.org/wiki/Wikidata_Query_Service/User_Manual#SPARQL_endpoint
+        */
+        $baseURL = 'https://query.wikidata.org/sparql?format=xml&query=';
+        $folder = "wikidata";
+        $inputExtension = "rq";
+        $outputExtension = "xml";
+        break;
+
+    case "wikidata-json":
+    case "wdj":
+        /*
+        https://www.mediawiki.org/wiki/Wikidata_Query_Service/User_Manual#SPARQL_endpoint
+        */
+        $baseURL = 'https://query.wikidata.org/sparql?format=json&query=';
+        $folder = "wikidata";
+        $inputExtension = "rq";
+        $outputExtension = "json";
+        break;
+
+    case "overpassql":
+    case "opq":
+        /* 
+        https://wiki.openstreetmap.org/wiki/Overpass_API#Public_Overpass_API_instances
+        https://wiki.openstreetmap.org/wiki/Overpass_API/Language_Guide
+        https://wiki.openstreetmap.org/wiki/Overpass_turbo/Extended_Overpass_Turbo_Queries
+        */
+        $baseURL = getOverpassEndpoint() . "?data=";
+        $folder = "overpassql";
+        $inputExtension = "overpass";
+        $outputExtension = "json";
+
+    case "overpassxml":
+    case "opx":
+        /*
+        https://wiki.openstreetmap.org/wiki/Overpass_API#Public_Overpass_API_instances
+        https://wiki.openstreetmap.org/wiki/Overpass_API/Language_Guide
+        */
+        $baseURL = getOverpassEndpoint() . "?data=";
+        $folder = "overpassxml";
+        $inputExtension = "xml";
+        $outputExtension = "json";
+
+    default:
+        echo "Please provide a VALID string as the first argument." . PHP_EOL;
+        exit(3);
 }
 
 $ret = 0;
 for ($i = 2; $i < $argc; $i++) {
-    if (empty($argv[$i]) || !is_numeric($argv[$i])) {
-        echo "Invalid number argument." . PHP_EOL;
+    if (empty($argv[$i])) {
+        echo "Invalid query name." . PHP_EOL;
         exit(2);
     }
-    $inputNumber = (int)$argv[$i];
+    $inputNumber = $argv[$i];
 
     $fileName = "samples/$folder/$inputNumber.$inputExtension";
     if (!file_exists($fileName)) {
