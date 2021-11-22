63810efa806fa0c2fb0cf5f3e5bd8a023fb0041c
Daniele Santini <danysan95@gmail.com>
2021-11-22 22:18:55 +0100

    Fix JSONLocalQueryResult constructor

--- web/app/result/JSONLocalQueryResult.php
+++ web/app/result/JSONLocalQueryResult.php
@@ -22,18 +22,23 @@ class JSONLocalQueryResult extends LocalQueryResult implements JSONQueryResult
      */
     public function __construct($success, $result, $sourcePath = null)
     {
-        if ($success && empty($result) && empty($sourcePath)) {
+        $emptyResult = empty($result) && $result !== [];
+        $resultFromResult = $success && !$emptyResult;
+        $resultFromSourcePath = $success && $emptyResult;
+
+        if ($resultFromSourcePath && empty($sourcePath)) {
             throw new \Exception("JSONLocalQueryResult: Empty result");
-        } elseif ($success && $result !== null && !is_string($result) && !is_array($result)) {
+        } elseif ($resultFromResult && !is_string($result) && !is_array($result)) {
             throw new \Exception("JSONLocalQueryResult: Result must be a string or array");
         }
+
         parent::__construct($success, $result, $sourcePath);
     }
 
     public function getJSONData(): array
     {
         $res = $this->getResult();
-        return is_array($res) ? $res : json_decode((string)$res);
+        return is_array($res) ? $res : (array)json_decode((string)$res, true);
     }
 
     public function getGeoJSONData(): array
