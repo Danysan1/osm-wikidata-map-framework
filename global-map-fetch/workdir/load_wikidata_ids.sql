7b8148edf365a58ffbfeec9192051e50ea3530f6
Daniele Santini <danysan95@gmail.com>
2021-11-28 10:51:32 +0100

    Sanitize input

--- global-map-fetch/workdir/load_wikidata_ids.sql
+++ global-map-fetch/workdir/load_wikidata_ids.sql
@@ -1,21 +1,26 @@
 INSERT INTO public.wikidata (wd_wikidata_id)
-SELECT REPLACE(value->'namedAfter'->>'value', 'http://www.wikidata.org/entity/', '')
-FROM json_array_elements(('__WIKIDATA_JSON__'::JSON)->'results'->'bindings')
-UNION
-SELECT REGEXP_SPLIT_TO_TABLE(tags->'subject:wikidata',';') FROM public.planet_osm_point
-UNION
-SELECT REGEXP_SPLIT_TO_TABLE(tags->'subject:wikidata',';') FROM public.planet_osm_line
-UNION
-SELECT REGEXP_SPLIT_TO_TABLE(tags->'subject:wikidata',';') FROM public.planet_osm_polygon
-UNION
-SELECT REGEXP_SPLIT_TO_TABLE(tags->'name:etymology:wikidata',';') FROM public.planet_osm_point
-UNION
-SELECT REGEXP_SPLIT_TO_TABLE(tags->'name:etymology:wikidata',';') FROM public.planet_osm_line
-UNION
-SELECT REGEXP_SPLIT_TO_TABLE(tags->'name:etymology:wikidata',';') FROM public.planet_osm_polygon;
+SELECT val
+FROM (
+	SELECT TRIM(REPLACE(value->'namedAfter'->>'value', 'http://www.wikidata.org/entity/', '')) as val
+	FROM json_array_elements(('__WIKIDATA_JSON__'::JSON)->'results'->'bindings')
+	UNION
+	SELECT TRIM(REGEXP_SPLIT_TO_TABLE(tags->'subject:wikidata',';')) as val FROM public.planet_osm_point
+	UNION
+	SELECT TRIM(REGEXP_SPLIT_TO_TABLE(tags->'subject:wikidata',';')) as val FROM public.planet_osm_line
+	UNION
+	SELECT TRIM(REGEXP_SPLIT_TO_TABLE(tags->'subject:wikidata',';')) as val FROM public.planet_osm_polygon
+	UNION
+	SELECT TRIM(REGEXP_SPLIT_TO_TABLE(tags->'name:etymology:wikidata',';')) as val FROM public.planet_osm_point
+	UNION
+	SELECT TRIM(REGEXP_SPLIT_TO_TABLE(tags->'name:etymology:wikidata',';')) as val FROM public.planet_osm_line
+	UNION
+	SELECT TRIM(REGEXP_SPLIT_TO_TABLE(tags->'name:etymology:wikidata',';')) as val FROM public.planet_osm_polygon
+) AS x
+WHERE LEFT(val,1)='Q';
 
 INSERT INTO public.wikidata_named_after (wna_wikidata_id, wna_named_after_wikidata_id)
 SELECT DISTINCT
 	REPLACE(value->'element'->>'value', 'http://www.wikidata.org/entity/', ''),
 	REPLACE(value->'namedAfter'->>'value', 'http://www.wikidata.org/entity/', '')
-FROM json_array_elements(('__WIKIDATA_JSON__'::JSON)->'results'->'bindings');
+FROM json_array_elements(('__WIKIDATA_JSON__'::JSON)->'results'->'bindings')
+WHERE LEFT(value->'namedAfter'->>'value', 31) = 'http://www.wikidata.org/entity/';
