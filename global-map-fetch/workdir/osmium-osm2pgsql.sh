79d0a5ddc6de66f6f5b8e05aa37bf8b242dbb439
Daniele Santini <danysan95@gmail.com>
2021-11-29 22:31:49 +0100

    WIP Postgis wikidata base init

--- global-map-fetch/workdir/osmium-osm2pgsql.sh
+++ global-map-fetch/workdir/osmium-osm2pgsql.sh
@@ -16,7 +16,13 @@ else
 fi
 
 echo '========================= Preparing DB schema ========================='
-psql -h "$2" -d osm -U osm -f 'postgis-setup.sql'
+if [ -n "$3" ] || [ $(psql -h "$2" -U osm -d osm -t -c "SELECT EXISTS (
+        SELECT FROM information_schema.tables 
+        WHERE  table_schema = 'public'
+        AND    table_name   = 'wikidata_text'
+    )" | xargs) = 'f' ]; then 
+    psql -h "$2" -d osm -U osm -f 'postgis-setup.sql'
+fi
 
 
 if [ -f 'get_wikidata_ids.tmp.json' ]; then
@@ -40,24 +46,24 @@ else
 fi
 
 
-if [ -f 'get_wikidata_base.tmp.json' ]; then 
-    echo '========================= Wikidata base data already downloaded ========================='
-else
-    echo '========================= Downloading Wikidata base data ========================='
-    WIKIDATA_IDS=$(psql -h "$2" -d osm -U osm -t -c "SELECT STRING_AGG('wd:'||wd_wikidata_id, ' ') FROM wikidata")
-    echo "s/__WIKIDATA_IDS__/$WIKIDATA_IDS/g" > get_wikidata_base.tmp.sed
-    sed -f 'get_wikidata_base.tmp.sed' 'get_wikidata_base.rq' > get_wikidata_base.tmp.rq
-    curl -X 'POST' --data-urlencode 'format=json' --data-urlencode 'query@get_wikidata_base.tmp.rq' -o 'get_wikidata_base.tmp.json' -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36" 'https://query.wikidata.org/sparql'
-fi
-
-if [ "$(psql -h "$2" -U osm -d osm -t -c "SELECT COUNT(wd_position) FROM wikidata" | xargs)" != '0' ]; then
+if [ "$(psql -h "$2" -U osm -d osm -t -c "SELECT COUNT(*) FROM wikidata WHERE wd_download_date IS NULL" | xargs)" == '0' ]; then
     echo '========================= Wikidata base data already loaded into DB ========================='
 else
-    echo '========================= Loading Wikidata base data into DB ========================='
-    WIKIDATA_JSON=$(cat get_wikidata_base.tmp.json | tr -d '\n' | sed -e "s/'/''/g" -e "s/|/\|/g")
-    echo "s|__WIKIDATA_JSON__|$WIKIDATA_JSON|g" > load_wikidata_base.tmp.sed
-    sed -f 'load_wikidata_base.tmp.sed' 'load_wikidata_base.sql' > load_wikidata_base.tmp.sql
-    psql -h "$2" -d osm -U osm -t -f 'load_wikidata_base.tmp.sql'
+    WIKIDATA_IDS=$(psql -h "$2" -d osm -U osm -t -c "SELECT STRING_AGG('wd:'||wd_wikidata_id, ' ') FROM (SELECT wd_wikidata_id FROM wikidata WHERE wd_download_date IS NULL LIMIT 1000) AS x" | xargs)
+    while [ -n "$WIKIDATA_IDS" ]; do
+        echo '========================= Downloading Wikidata base data ========================='
+        echo "s/__WIKIDATA_IDS__/$WIKIDATA_IDS/g" > get_wikidata_base.tmp.sed
+        sed -f 'get_wikidata_base.tmp.sed' 'get_wikidata_base.rq' > get_wikidata_base.tmp.rq
+        curl -X 'POST' --data-urlencode 'format=json' --data-urlencode 'query@get_wikidata_base.tmp.rq' -o 'get_wikidata_base.tmp.json' -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36" 'https://query.wikidata.org/sparql'
+        
+        echo '========================= Loading Wikidata base data into DB ========================='
+        WIKIDATA_JSON=$(cat get_wikidata_base.tmp.json | tr -d '\n' | sed -e "s/'/''/g" -e "s/|/\|/g")
+        echo "s|__WIKIDATA_JSON__|$WIKIDATA_JSON|g" > load_wikidata_base.tmp.sed
+        sed -f 'load_wikidata_base.tmp.sed' 'load_wikidata_base.sql' > load_wikidata_base.tmp.sql
+        psql -h "$2" -d osm -U osm -t -f 'load_wikidata_base.tmp.sql'
+
+        WIKIDATA_IDS=$(psql -h "$2" -d osm -U osm -t -c "SELECT STRING_AGG('wd:'||wd_wikidata_id, ' ') FROM (SELECT wd_wikidata_id FROM wikidata WHERE wd_download_date IS NULL LIMIT 1000) AS x" | xargs)
+    done
 fi
 
 
@@ -65,7 +71,7 @@ echo '========================= Elaborating etymology data =====================
 psql -h "$2" -d osm -U osm -f 'osmium-osm2pgsql-convert.sql'
 
 
-if [ -f 'global-map.geojson' ]; then 
+if [ -f 'global-map.tmp.geojson' ]; then 
     echo '========================= Global map already generated ========================='
 else
     echo '========================= Generating global map ========================='
@@ -77,7 +83,7 @@ if [ -f 'get_wikidata_text.tmp.json' ]; then
     echo '========================= Wikidata text initializazion data already downloaded ========================='
 else
     echo '========================= Downloading Wikidata  text initializazion data ========================='
-    WIKIDATA_IDS=$(psql -h "$2" -d osm -U osm -t -c "SELECT STRING_AGG('wd:'||wd_wikidata_id, ' ') FROM wikidata LIMIT 1000")
+    WIKIDATA_IDS=$(psql -h "$2" -d osm -U osm -t -c "SELECT STRING_AGG('wd:'||wd_wikidata_id, ' ') FROM wikidata LIMIT 500")
     echo "s/__WIKIDATA_IDS__/$WIKIDATA_IDS/g" > get_wikidata_text.tmp.sed
     sed -e "s/__LANGUAGE__/'it'/" -f 'get_wikidata_text.tmp.sed' 'get_wikidata_text.rq' > get_wikidata_text.tmp.rq
     curl -X 'POST' --data-urlencode 'format=json' --data-urlencode 'query@get_wikidata_text.tmp.rq' -o 'get_wikidata_text.tmp.json' -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36" 'https://query.wikidata.org/sparql'
