2bc9540244af950881d8003060e51714546f0f6d
Daniele Santini <danysan95@gmail.com>
2021-12-05 11:02:47 +0100

    Wait for DB

--- init-workdir/osmium-postgis-base.sh
+++ init-workdir/osmium-postgis-base.sh
@@ -13,12 +13,24 @@ echo '========================= Checking DB ========================='
 if ! command -v psql > /dev/null; then
     echo 'ERROR: psql is not installed'
     exit 60
-elif ! psql -h "$2" -U osm -d osm -c 'SELECT version()' > /dev/null ; then
-    echo "ERROR: could not connect to PostgreSQL database on '$2'"
-    exit 70
-#elif ! psql -h "$2" -U osm -d osm -c 'SELECT PostGIS_Version()' > /dev/null ; then
-#    echo 'ERROR: PostGIS is required, it is not installed on the DB'
-#    exit 80
 fi
 
+# https://docs.docker.com/compose/startup-order/
+CONNECTION_TRIES=0
+until psql -h "$2" -U osm -d osm -c 'SELECT version()'; do
+    >&2 echo "Postgres is unavailable - sleeping"
+    ((CONNECTION_TRIES+=1))
+    if [ $CONNECTION_TRIES -gt 60 ]; then
+        echo "ERROR: max connection tries reached, could not connect to PostgreSQL database on '$2'"
+        exit 70
+    fi
+    sleep 1
+done
+
+echo '========================= Checking PostGIS ========================='
 psql -h "$2" -U osm -d osm -f 'setup-postgis.sql'
+
+if ! psql -h "$2" -U osm -d osm -c 'SELECT PostGIS_Version()' ; then
+    echo 'ERROR: PostGIS is required, it is not installed on the DB and initialization failed'
+    exit 80
+fi
