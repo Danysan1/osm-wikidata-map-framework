7fe80b2a2c6fa01e87c90626a58a16d1f7af53fb
Daniele Santini <danysan95+gitlab@gmail.com>
2021-12-24 07:12:46 +0000

    WIPD DB init

--- web/db_init.php
+++ web/db_init.php
@@ -340,7 +340,44 @@ if ($use_db) {
                 $dbname = (string)$conf->get("db-database");
                 $user = (string)$conf->get("db-user");
                 $password = (string)$conf->get("db-password");
+                echo '========================= Loading data into DB... =========================' . PHP_EOL;
                 execAndCheck("PGPASSWORD='$password' osm2pgsql --host='$host' --port='$port' --database='$dbname' --user='$user' --hstore --proj=4326 --create --slim --flat-nodes=/tmp/osm2pgsql-nodes.cache --cache=0 '$filteredFile'");
+                echo '========================= Data loaded into DB =========================' . PHP_EOL;
+
+                echo '========================= Converting elements... =========================' . PHP_EOL;
+                $n_element = $dbh->exec(
+                    "INSERT INTO element (
+                        el_osm_type,
+                        el_osm_id,
+                        el_name,
+                        el_wikidata,
+                        el_subject_wikidata,
+                        el_name_etymology_wikidata,
+                        el_geometry)
+                    SELECT 'node', osm_id, name, tags->'wikidata', tags->'subject:wikidata', tags->'name:etymology:wikidata', way
+                    FROM planet_osm_point
+                    UNION
+                    SELECT
+                        CASE WHEN osm_id > 0 THEN 'way' ELSE 'relation' END AS osm_type,
+                        CASE WHEN osm_id > 0 THEN osm_id ELSE -osm_id END AS osm_id,
+                        name,
+                        tags->'wikidata',
+                        tags->'subject:wikidata',
+                        tags->'name:etymology:wikidata',
+                        way AS geom
+                    FROM planet_osm_line
+                    UNION
+                    SELECT
+                        CASE WHEN osm_id > 0 THEN 'way' ELSE 'relation' END AS osm_type,
+                        CASE WHEN osm_id > 0 THEN osm_id ELSE -osm_id END AS osm_id,
+                        name,
+                        tags->'wikidata',
+                        tags->'subject:wikidata',
+                        tags->'name:etymology:wikidata',
+                        way AS geom
+                    FROM planet_osm_polygon"
+                );
+                echo "========================= Converted $n_element elements =========================" . PHP_EOL;
             }
         }
 
