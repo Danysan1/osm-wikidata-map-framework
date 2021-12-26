d7b5d2f806cf0b2c23328df933656a0669610bbd
Daniele Santini <danysan95@gmail.com>
2021-12-26 20:51:01 +0100

    Improve conf comments

--- web/app/PostGIS_PDO.php
+++ web/app/PostGIS_PDO.php
@@ -14,10 +14,13 @@ class PostGIS_PDO extends PDO
         $host = (string)$conf->get("db-host");
         $port = (int)$conf->get("db-port");
         $dbname = (string)$conf->get("db-database");
+        $user = (string)$conf->get("db-user");
+        $password = (string)$conf->get("db-password");
+        // Test $db = \pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
         parent::__construct(
             "pgsql:host=$host;port=$port;dbname=$dbname",
-            (string)$conf->get("db-user"),
-            (string)$conf->get("db-password"),
+            $user,
+            $password,
             [
                 PDO::ATTR_EMULATE_PREPARES => false, // https://websitebeaver.com/php-pdo-prepared-statements-to-prevent-sql-injection
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
