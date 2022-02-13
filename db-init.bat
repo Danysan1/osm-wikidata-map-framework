4cc1f890d291c258f05f082c4a03af7737624085
Daniele Santini <danysan95@gmail.com>
2022-02-13 17:32:30 +0100

    Fix db-ini.bat

--- db-init.bat
+++ db-init.bat
@@ -2,6 +2,6 @@
 
 docker-compose up -d
 
-set source=%1
+set source="%~1"
 set sourceFile=%source:.\web\=%
 docker-compose exec web php db-init.php %sourceFile% %2 %3
--- db-init.sh
+++ db-init.sh
@@ -2,6 +2,6 @@
 
 docker-compose up -d
 
-fullSource=$1
-sourceFile=$(echo "$fullSource" | sed 's:./web/::')
+source="$1"
+sourceFile=$(echo "$source" | sed 's:./web/::')
 docker-compose exec web php db-init.php $sourceFile $2 $3
