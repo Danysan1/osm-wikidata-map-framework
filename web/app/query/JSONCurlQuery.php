1316b667493ca7a52fc072dd5480eb518de30fc3
Daniele Santini <danysan95@gmail.com>
2022-03-17 15:07:23 +0100

    Fix CurlQuery import

--- web/app/query/JSONCurlQuery.php
+++ web/app/query/JSONCurlQuery.php
@@ -3,10 +3,12 @@
 namespace App\Query;
 
 require_once(__DIR__ . "/JSONQuery.php");
+require_once(__DIR__ . "/CurlQuery.php");
 require_once(__DIR__ . "/../result/JSONQueryResult.php");
 require_once(__DIR__ . "/../result/JSONRemoteQueryResult.php");
 
 use App\Query\JSONQuery;
+use App\Query\CurlQuery;
 use App\Result\JSONQueryResult;
 use App\Result\QueryResult;
 use App\Result\JSONRemoteQueryResult;
