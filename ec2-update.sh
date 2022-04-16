ff9748a995f4ce08a0ac2d1d8830167fa802fc25
Daniele Santini <danysan95@gmail.com>
2022-04-16 14:29:02 +0200

    Fix ec2-update for crontab

--- ec2-update.sh
+++ ec2-update.sh
@@ -2,7 +2,7 @@
 cd $(dirname "$0")
 git fetch
 git pull
-docker-compose --profile 'prod' pull
-#docker-compose --profile 'prod' build
-docker-compose --profile 'prod' up -d
+/usr/local/bin/docker-compose --profile 'prod' pull
+/usr/local/bin/docker-compose --profile 'prod' build
+/usr/local/bin/docker-compose --profile 'prod' up -d
 
