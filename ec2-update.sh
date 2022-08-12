e7fdc7d291d2c3dd85f77737f37e18945690f5a3
Daniele Santini <danysan95@gmail.com>
2022-08-12 22:12:07 +0200

    Improve ec2-update

--- ec2-update.sh
+++ ec2-update.sh
@@ -4,4 +4,4 @@ cd $(dirname "$0")
 /usr/bin/git fetch
 /usr/bin/git pull
 /usr/local/bin/docker-compose --profile 'prod' pull
-/usr/local/bin/docker-compose --profile 'prod' up --build --detach
+/usr/local/bin/docker-compose --profile 'prod' up --build --detach --always-recreate-deps
