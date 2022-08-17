56f6bb5de44f35ca19211c2c4f61afb6b291f170
Daniele Santini <danysan95@gmail.com>
2022-08-17 16:15:13 +0200

    Fix wrong propagations

--- web/db-init.php
+++ web/db-init.php
@@ -784,10 +784,10 @@ function propagateEtymologies(PDO $dbh, int $depth = 1): int
             FROM oem.etymology AS old_et
             JOIN oem.osmdata AS old_el
                 ON old_et.et_el_id = old_el.osm_id
-                AND old_el.osm_tags ?? 'name' -- As of PHP 7.4.0, question marks can be escaped by doubling them. That means that the ?? string will be translated to ? when sending the query to the database.
+                AND old_el.osm_tags ?? 'highway' -- As of PHP 7.4.0, question marks can be escaped by doubling them. That means that the ?? string will be translated to ? when sending the query to the database.
             JOIN oem.osmdata AS new_el
                 ON old_el.osm_id < new_el.osm_id
-                AND new_el.osm_tags ?? 'name'
+                AND new_el.osm_tags ?? 'highway'
                 AND old_el.osm_tags->'name' = new_el.osm_tags->'name'
                 AND ST_Intersects(old_el.osm_geometry, new_el.osm_geometry)
             LEFT JOIN oem.etymology AS new_et ON new_et.et_el_id = new_el.osm_id
