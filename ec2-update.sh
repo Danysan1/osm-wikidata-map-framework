3cef5f8bf881ab0fe7d53b11edde8320c55058ee
Daniele Santini <danysan95@gmail.com>
2022-08-28 22:18:05 +0200

    docker-compose push

--- ec2-update.sh
+++ ec2-update.sh
@@ -5,3 +5,4 @@ cd $(dirname "$0")
 /usr/bin/git pull
 /usr/local/bin/docker-compose --profile 'prod' pull
 /usr/local/bin/docker-compose --profile 'prod' up --build --detach --always-recreate-deps
+/usr/local/bin/docker-compose --profile 'prod' push  # Upload arm builds, not buildable with CI
