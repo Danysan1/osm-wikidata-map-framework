730c0ccd724f0b462cfff6e5899ae22b1fafa0bc
Daniele Santini <danysan95@gmail.com>
2022-09-05 23:01:32 +0200

    Improve the global map

--- init/sql/global-map.sql
+++ init/sql/global-map.sql
@@ -2,6 +2,5 @@ CREATE MATERIALIZED VIEW oem.vm_global_map AS
 SELECT
     ST_ReducePrecision(ST_Centroid(el_geometry), 0.1) AS geom,
     COUNT(DISTINCT COALESCE(el_tags->>'name', el_id::TEXT)) AS num
-FROM oem.etymology
-JOIN oem.element ON et_el_id = el_id
+FROM oem.element
 GROUP BY geom;
