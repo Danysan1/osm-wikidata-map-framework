4cff9c12bfd72c859fb4a793f5d8fd2482670c6d
Daniele Santini <danysan95@gmail.com>
2022-09-25 21:13:56 +0200

    Improve cleanup pipeline

--- init/db-init.py
+++ init/db-init.py
@@ -1091,7 +1091,6 @@ kosovo_latest = OemDbInitDAG(
     ffwd_to_load=False,
     pbf_url="http://download.geofabrik.de/europe/kosovo-latest.osm.pbf"
 )
-
 kosovo_html = OemDbInitDAG(
     dag_id="db-init-kosovo-from-html",
     schedule_interval=None,
@@ -1103,15 +1102,26 @@ kosovo_html = OemDbInitDAG(
 
 with DAG(
     dag_id="db-init-cleanup",
-    schedule_interval=None,
+    schedule_interval="@daily",
     start_date=datetime(year=2022, month=9, day=15, tz=dagTimezone),
     catchup=False,
     tags=['oem', 'db-init'],
 ) as dag:
-    BashOperator(
+    task_ls = BashOperator(
         task_id="ls",
-        bash_command="""
-            ls -lah /workdir/
-            rm -r /workdir/*
-        """,
+        bash_command="ls -lhR /workdir/",
+        doc_md="""
+            # List all files in the work directory
+        """
     )
+    
+    task_cleanup = BashOperator(
+        task_id="cleanup",
+        bash_command="find /workdir/*/* -mtime 8 -exec rm -r {} \;",
+        doc_md="""
+            # Cleanup old files from work directory
+
+            Remove files and DAG run folders older than 8 days
+        """
+    )
+    task_ls >> task_cleanup
