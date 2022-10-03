0e34c4690f7e1d199fecce5debb749d0f3786200
Daniele Santini <danysan95@gmail.com>
2022-10-03 12:42:15 +0200

    PostGIS_PDO minor refactoring

--- web/app/PostGIS_PDO.php
+++ web/app/PostGIS_PDO.php
@@ -21,25 +21,22 @@ class PostGIS_PDO extends PDO
         if (!$conf->getBool("db_enable"))
             throw new Exception("The usage of the DB is disabled in the configuration");
 
-        $host = $host ?: (string)$conf->get("db_host");
-        $port = $port ?: (int)$conf->get("db_port");
-        $user = $user ?: (string)$conf->get("db_user");
-        $password = $password ?: (string)$conf->get("db_password");
-
-        $server = $_SERVER["SERVER_NAME"];
         if ($dbname == null && $conf->has("db_database_list")) {
+            $server = (string)$_SERVER["SERVER_NAME"];
             $dbname_array = json_decode((string)$conf->get("db_database_list"), true);
             if (!is_array($dbname_array))
                 throw new Exception("Bad db_database_list configuration");
             if (!empty($dbname_array[$server])) {
-                $dbname = $dbname_array[$server];
+                $dbname = (string)$dbname_array[$server];
                 error_log("Using DB name '$dbname' for server '$server'");
             }
         }
 
-        if ($dbname == null) {
-            $dbname = (string)$conf->get("db_database");
-        }
+        $host = $host ?: (string)$conf->get("db_host");
+        $port = $port ?: (int)$conf->get("db_port");
+        $dbname = $dbname ?: (string)$conf->get("db_database");
+        $user = $user ?: (string)$conf->get("db_user");
+        $password = $password ?: (string)$conf->get("db_password");
 
         parent::__construct(
             "pgsql:host=$host;port=$port;dbname=$dbname;options=--application_name=open_etymology_map",
