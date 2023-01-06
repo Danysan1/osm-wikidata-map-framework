6acb2021f39a5f80a95398a136cb5dce87a68ea1
Daniele Santini <danysan95@gmail.com>
2023-01-06 20:18:51 +0100

    Improve docs

--- airflow-dags/OsmPbfDownloadDAG.py
+++ airflow-dags/OsmPbfDownloadDAG.py
@@ -87,7 +87,48 @@ def check_whether_to_procede(date_path, ti:TaskInstance, **context) -> bool:
 
 def start_torrent_download(ti:TaskInstance):
     """
-    See https://transmission-rpc.readthedocs.io/en/v3.4.0/client.html
+    ## Download the PBF source file through torrent
+
+    Start the download of the source PBF file from the torrent URL calculated by get_source_url.
+    
+    Links:
+    * [transmission-rpc Client dcumentation](https://transmission-rpc.readthedocs.io/en/v3.4.0/client.html)
+
+    
+    Torrent download method explored before choosing this one:
+
+    1. Download using DockerOperator + lftp
+        * Docker image: `minidocks/lftp:latest`
+        * Command: `lftp -c torrent -O '{dest_folder}' '{torrent_url}'`
+        * Documentation: http://lftp.yar.ru/lftp-man.html
+        * Problems:
+            * it fails with 'Not saving nodes, DHT not ready'
+    2. Download using DockerOperator + aria2c
+        * Docker image: `207m/aria2c:latest`
+        * Command: `aria2c --dir '{dest_folder}' '{torrent_url}'`
+        * Documentation: https://aria2.github.io/manual/en/html/aria2c.html#bittorrent-metalink-options
+        * Problems:
+            * it fails
+    3. Download using DockerOperator + transmission-cli
+        * Docker image: `mikesplain/transmission-cli`
+        * Command: `transmission-cli --download-dir '{dest_folder}' '{torrent_url}'`
+        * Documentation: https://manpages.ubuntu.com/manpages/bionic/man1/transmission-cli.1.html
+        * Problems:
+            * transmission-cli is deprecated in favor of transmission-remote
+            * it fails with 'Not saving nodes, DHT not ready'
+            * even if it fails it returns a success return code
+    4. Download using DockerOperator + transmission-remote (+ transmission-daemon)
+        * Docker image: `linuxserver/transmission`
+        * Command: `transmission-remote torrent-daemon:9091 --download-dir '{dest_folder}' --add '{torrent_url}'`
+        * Documentation: https://linux.die.net/man/1/transmission-remote
+        * Notes:
+            * requires transmission-daemon (see torrent-daemon service in docker-compose.yml)
+        * Problems:
+            * adds the torrent to the download queue but doesn't return the torrent id, making it really hard to check the status
+    5. Download using PythonOperator + transmission-rpc (+ transmission-daemon)
+        * Current implementation
+        * Notes:
+            * requires transmission-daemon (see torrent-daemon service in docker-compose.yml)
     """
     from transmission_rpc import Client
     download_dir = ti.xcom_pull(task_ids='get_source_url', key='work_dir')
@@ -98,8 +139,11 @@ def start_torrent_download(ti:TaskInstance):
 
 def check_if_torrent_is_complete(ti:TaskInstance) -> bool:
     """
-    See https://transmission-rpc.readthedocs.io/en/v3.4.0/client.html
-    See https://transmission-rpc.readthedocs.io/en/v3.4.0/torrent.html
+    ## Check whether the torrent has finished downloading
+    
+    Links:
+    * [transmission-rpc Client dcumentation](https://transmission-rpc.readthedocs.io/en/v3.4.0/client.html)
+    * [transmission-rpc Torrent dcumentation](https://transmission-rpc.readthedocs.io/en/v3.4.0/torrent.html)
     """
     from transmission_rpc import Client
     torrent_id = ti.xcom_pull(task_ids='download_torrent', key='torrent_id')
@@ -224,46 +268,7 @@ class OsmPbfDownloadDAG(DAG):
             task_id = "download_torrent",
             python_callable = start_torrent_download,
             dag = self,
-            doc_md=dedent("""
-                # Download the PBF source file through torrent
-
-                Start the download of the source PBF file from the torrent URL calculated by get_source_url.
-
-                Explored torrent download options:
-
-                1. Download using DockerOperator + lftp
-                    * Docker image: `minidocks/lftp:latest`
-                    * Command: `lftp -c torrent -O '{dest_folder}' '{torrent_url}'`
-                    * Documentation: http://lftp.yar.ru/lftp-man.html
-                    * Problems:
-                        * it fails with 'Not saving nodes, DHT not ready'
-                2. Download using DockerOperator + aria2c
-                    * Docker image: `207m/aria2c:latest`
-                    * Command: `aria2c --dir '{dest_folder}' '{torrent_url}'`
-                    * Documentation: https://aria2.github.io/manual/en/html/aria2c.html#bittorrent-metalink-options
-                    * Problems:
-                        * it fails
-                3. Download using DockerOperator + transmission-cli
-                    * Docker image: `mikesplain/transmission-cli`
-                    * Command: `transmission-cli --download-dir '{dest_folder}' '{torrent_url}'`
-                    * Documentation: https://manpages.ubuntu.com/manpages/bionic/man1/transmission-cli.1.html
-                    * Problems:
-                        * transmission-cli is deprecated in favor of transmission-remote
-                        * it fails with 'Not saving nodes, DHT not ready'
-                        * even if it fails it returns a success return code
-                4. Download using DockerOperator + transmission-remote (+ transmission-daemon)
-                    * Docker image: `linuxserver/transmission`
-                    * Command: `transmission-remote torrent-daemon:9091 --download-dir '{dest_folder}' --add '{torrent_url}'`
-                    * Documentation: https://linux.die.net/man/1/transmission-remote
-                    * Notes:
-                        * requires transmission-daemon (see torrent-daemon service in docker-compose.yml)
-                    * Problems:
-                        * adds the torrent to the download queue but doesn't return the torrent id, making it really hard to check the status
-                5. Download using PythonOperator + transmission-rpc (+ transmission-daemon)
-                    * Current implementation
-                    * Notes:
-                        * requires transmission-daemon (see torrent-daemon service in docker-compose.yml)
-            """)
+            doc_md=dedent(start_torrent_download.__doc__)
         )
         task_chose_download_method >> task_download_torrent
 
