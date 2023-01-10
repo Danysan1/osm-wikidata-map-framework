0c9f8adb2496382231bce558487d474b07717b30
Daniele Santini <danysan95@gmail.com>
2023-01-10 09:49:09 +0100

    Fix broken URL

--- public/index.php
+++ public/index.php
@@ -121,7 +121,7 @@ if (
                 <input type="hidden" name="item_name" value="This donation will help Open Etymology Map to stay up and running. Thank you!" />
                 <input type="hidden" name="currency_code" value="EUR" />
                 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-                <img alt="" border="0" src="https://www.paypal.com/en_IT/i/scr/pixel.gif" width="1" height="1" />
+                <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
             </form>
             <h3>Click anywhere on the map to explore.</h3>
         </div>
