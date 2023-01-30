10825d7b7a47ba6db42445c42172e89b5c09a653
Daniele Santini <danysan95@gmail.com>
2023-01-31 00:12:33 +0100

    Add index to osmdata tags

--- airflow/dags/db-init-europe.py
+++ airflow/dags/db-init-europe.py
@@ -4,7 +4,7 @@ from OemDbInitDAG import OemDbInitDAG
 from airflow.models import DAG #! Don't delete, necessary for Airflow to recognize this file as containing DAGs
 
 download_europe_pbf = OsmPbfDownloadDAG(
-    dag_id="db-init-europe-latest",
+    dag_id="download-europe-latest",
     schedule=None,
     pbf_url="http://download.geofabrik.de/europe-latest.osm.pbf",
     prefix="europe"
--- airflow/dags/sql/remove-elements-too-big.sql
+++ airflow/dags/sql/remove-elements-too-big.sql
@@ -1,3 +1,5 @@
 DELETE FROM oem.osmdata
 WHERE (osm_tags ? 'name:etymology:wikidata' AND osm_tags ? 'wikidata' AND osm_tags->>'name:etymology:wikidata' = osm_tags->>'wikidata')
-OR ST_Area(osm_geometry) >= 0.01 -- EPSG 4326 => 0.01 square degrees
+OR ST_Area(osm_geometry) >= 0.005; -- EPSG 4326 => 0.005 square degrees
+
+CREATE INDEX osmdata_tags_idx ON oem.osmdata USING GIN (osm_tags);
