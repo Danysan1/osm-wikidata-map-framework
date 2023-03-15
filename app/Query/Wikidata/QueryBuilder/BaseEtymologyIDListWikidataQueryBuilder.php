dadfd40a44b5f5e65d2f71f7cc4d2cf77565ed0f
Daniele Santini <danysan95@gmail.com>
2023-03-15 09:33:05 +0100

    Move date download to backend for century stats

--- app/Query/Wikidata/QueryBuilder/BaseEtymologyIDListWikidataQueryBuilder.php
+++ app/Query/Wikidata/QueryBuilder/BaseEtymologyIDListWikidataQueryBuilder.php
@@ -13,19 +13,62 @@ class BaseEtymologyIDListWikidataQueryBuilder extends BaseIDListWikidataQueryBui
                 (SAMPLE(?instance_name) AS ?instance)
                 (SAMPLE(?genderID) AS ?genderID)
                 (SAMPLE(?gender_name) AS ?gender)
+                (SAMPLE(?event_date) AS ?event_date)
+                (SAMPLE(?event_date_precision) AS ?event_date_precision)
+                (SAMPLE(?start_date) AS ?start_date)
+                (SAMPLE(?start_date_precision) AS ?start_date_precision)
+                (SAMPLE(?birth_date) AS ?birth_date)
+                (SAMPLE(?birth_date_precision) AS ?birth_date_precision)
             WHERE {
                 VALUES ?wikidata { $wikidataValues }
 
-                OPTIONAL { # instance of - https://www.wikidata.org/wiki/Property:P31
+                OPTIONAL { # necessary for type stats
                     ?instanceID ^wdt:P31 ?wikidata;
                         rdfs:label ?instance_name.
                     FILTER(lang(?instance_name)='$language').
                 }
 
-                OPTIONAL {
+                OPTIONAL { # necessary for gender stats
                     ?genderID ^wdt:P21 ?wikidata;
                         rdfs:label ?gender_name.
                     FILTER(lang(?gender_name)='$language').
+                }
+
+                OPTIONAL { # necessary for century stats
+                    ?wikidata p:P585/psv:P585 [ # event date - https://www.wikidata.org/wiki/Property:P585
+                        wikibase:timePrecision ?event_date_precision;
+                        wikibase:timeValue ?event_date
+                    ].
+                    MINUS {
+                        ?wikidata p:P585/psv:P585/wikibase:timePrecision ?other_event_date_precision.
+                        FILTER (?other_event_date_precision > ?event_date_precision).
+                    }.
+                }
+
+                # start time - https://www.wikidata.org/wiki/Property:P580
+                # inception - https://www.wikidata.org/wiki/Property:P571
+                # date of official opening - https://www.wikidata.org/wiki/Property:P1619
+                # necessary for century stats
+                OPTIONAL {
+                    ?wikidata (p:P580/psv:P580)|(p:P571/psv:P571)|(p:P1619/psv:P1619) [
+                        wikibase:timePrecision ?start_date_precision;
+                        wikibase:timeValue ?start_date
+                    ].
+                    MINUS {
+                        ?wikidata (p:P571/psv:P571/wikibase:timePrecision)|(p:P1619/psv:P1619/wikibase:timePrecision)|(p:P569/psv:P569/wikibase:timePrecision) ?other_start_date_precision.
+                        FILTER (?other_start_date_precision > ?start_date_precision).
+                    }.
+                }
+
+                OPTIONAL { # necessary for century stats
+                    ?wikidata p:P569/psv:P569 [ # birth date - https://www.wikidata.org/wiki/Property:P569
+                        wikibase:timePrecision ?birth_date_precision;
+                        wikibase:timeValue ?birth_date
+                    ].
+                    MINUS {
+                        ?wikidata p:P569/psv:P569/wikibase:timePrecision ?other_birth_date_precision.
+                        FILTER (?other_birth_date_precision > ?birth_date_precision).
+                    }.
                 }
             }
             GROUP BY ?wikidata";
--- src/services/WikidataService.ts
+++ src/services/WikidataService.ts
@@ -26,14 +26,8 @@ SELECT ?wikidata
     (GROUP_CONCAT(DISTINCT ?citizenship_name; SEPARATOR=', ') AS ?citizenship)
     (GROUP_CONCAT(DISTINCT ?picture; SEPARATOR='||') AS ?pictures)
     (GROUP_CONCAT(DISTINCT ?prize_name; SEPARATOR=', ') AS ?prizes)
-    (SAMPLE(?event_date) AS ?event_date)
-    (SAMPLE(?event_date_precision) AS ?event_date_precision)
-    (SAMPLE(?start_date) AS ?start_date)
-    (SAMPLE(?start_date_precision) AS ?start_date_precision)
     (SAMPLE(?end_date) AS ?end_date)
     (SAMPLE(?end_date_precision) AS ?end_date_precision)
-    (SAMPLE(?birth_date) AS ?birth_date)
-    (SAMPLE(?birth_date_precision) AS ?birth_date_precision)
     (SAMPLE(?death_date) AS ?death_date)
     (SAMPLE(?death_date_precision) AS ?death_date_precision)
     (GROUP_CONCAT(DISTINCT ?event_place_name; SEPARATOR=', ') AS ?event_place)
@@ -112,31 +106,6 @@ WHERE {
     }
 
     OPTIONAL {
-        ?wikidata p:P585/psv:P585 [ # event date - https://www.wikidata.org/wiki/Property:P585
-            wikibase:timePrecision ?event_date_precision;
-            wikibase:timeValue ?event_date
-        ].
-        MINUS {
-            ?wikidata p:P585/psv:P585/wikibase:timePrecision ?other_event_date_precision.
-            FILTER (?other_event_date_precision > ?event_date_precision).
-        }.
-    }
-
-    OPTIONAL {
-        ?wikidata (p:P580/psv:P580)|(p:P571/psv:P571)|(p:P1619/psv:P1619) [
-# start time - https://www.wikidata.org/wiki/Property:P580
-# inception - https://www.wikidata.org/wiki/Property:P571
-# date of official opening - https://www.wikidata.org/wiki/Property:P1619
-            wikibase:timePrecision ?start_date_precision;
-            wikibase:timeValue ?start_date
-        ].
-        MINUS {
-            ?wikidata (p:P571/psv:P571/wikibase:timePrecision)|(p:P1619/psv:P1619/wikibase:timePrecision)|(p:P569/psv:P569/wikibase:timePrecision) ?other_start_date_precision.
-            FILTER (?other_start_date_precision > ?start_date_precision).
-        }.
-    }
-
-    OPTIONAL {
         ?wikidata (p:P582/psv:P582)|(p:P576/psv:P576)|(p:P3999/psv:P3999) [
 # end time - https://www.wikidata.org/wiki/Property:P582
 # dissolved, abolished or demolished date - https://www.wikidata.org/wiki/Property:P576
@@ -147,17 +116,6 @@ WHERE {
         MINUS {
             ?wikidata (p:P582/psv:P582/wikibase:timePrecision)|(p:P576/psv:P576/wikibase:timePrecision)|(p:P3999/psv:P3999/wikibase:timePrecision) ?other_end_date_precision.
             FILTER (?other_end_date_precision > ?end_date_precision).
-        }.
-    }
-
-    OPTIONAL {
-        ?wikidata p:P569/psv:P569 [ # birth date - https://www.wikidata.org/wiki/Property:P569
-            wikibase:timePrecision ?birth_date_precision;
-            wikibase:timeValue ?birth_date
-        ].
-        MINUS {
-            ?wikidata p:P569/psv:P569/wikibase:timePrecision ?other_birth_date_precision.
-            FILTER (?other_birth_date_precision > ?birth_date_precision).
         }.
     }
 
