3373a5ca34b5ca209d928fa6abc7decffdfb8719
Daniele Santini <danysan95@gmail.com>
2023-04-07 11:38:50 +0200

    Fix osm_wikidata DB sources

--- app/loadWikidataReverseRelatedEntities.php
+++ app/loadWikidataReverseRelatedEntities.php
@@ -19,7 +19,7 @@ App\loadWikidataRelatedEntities(
     "ew_wikidata_cod",
     "NOT ew_from_osm",
     "et_el_id, et_wd_id, et_from_el_id, et_from_osm, et_from_key_ids, et_from_osm_wikidata_wd_id, et_from_osm_wikidata_prop_cod",
-    "ew_el_id, w2.wd_id, ew_el_id, FALSE, ARRAY['db_osm_wikidata_reverse'], w1.wd_id, REPLACE(value->'prop'->>'value', 'http://www.wikidata.org/prop/', '')",
+    "ew_el_id, w2.wd_id, ew_el_id, FALSE, ARRAY['osm_wikidata_reverse'], w1.wd_id, REPLACE(value->'prop'->>'value', 'http://www.wikidata.org/prop/', '')",
     "JOIN oem.element_wikidata_cods ON ew_wikidata_cod = w1.wd_wikidata_cod",
     "wikidata_reverse",
     [$wikidataProperty],
