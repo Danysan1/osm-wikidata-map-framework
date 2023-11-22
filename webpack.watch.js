c3fbd12e44a3b8f2ad4dcdc3a304f13ee2d642cf
Daniele Santini <danysan95@gmail.com>
2023-11-22 23:01:56 +0100

    Improve webpack config

--- webpack.watch.js
+++ webpack.watch.js
@@ -1,8 +1,10 @@
 const { merge } = require("webpack-merge"); // https://webpack.js.org/guides/production/
 const common = require("./webpack.common.js");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "cheap-source-map",
-  watch: false,
+  watch: true,
+  plugins: [new BundleAnalyzerPlugin()],
 });
