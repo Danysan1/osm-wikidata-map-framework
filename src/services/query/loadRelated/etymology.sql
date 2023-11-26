4566d80f43b5c605306c3927c16a0945528ff37c
Daniele Santini <danysan95@gmail.com>
2023-11-26 11:52:15 +0100

    Build load-related

--- src/services/query/loadRelated/etymology.sql
+++ src/services/query/loadRelated/etymology.sql
@@ -6,10 +6,10 @@ SELECT
     wd_id,
     ARRAY['wd_direct'],
     wd_id,
-    REPLACE(value->'from_prop'->>'value', 'http://www.wikidata.org/prop/', '')
+    REPLACE(value->'from_prop'->>'value', 'http://www.wikidata.org/prop/direct/', '')
 FROM json_array_elements(($1::JSON)->'results'->'bindings')
 JOIN owmf.osmdata
     ON osm_wikidata_cod = REPLACE(value->'item'->>'value', 'http://www.wikidata.org/entity/', '')
 JOIN owmf.wikidata ON wd_wikidata_cod = REPLACE(value->'etymology'->>'value', 'http://www.wikidata.org/entity/', '')
-WHERE LEFT(value->'related'->>'value', 31) = 'http://www.wikidata.org/entity/'
+WHERE value->'etymology'->>'value' LIKE 'http://www.wikidata.org/entity/%'
 ON CONFLICT (et_el_id, et_wd_id) DO NOTHING
