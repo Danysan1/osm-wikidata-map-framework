aa1f882a7578aebf7276979f4668f88582d4accd
Daniele Santini <danysan95@gmail.com>
2024-01-18 23:17:53 +0100

    Fix build error

--- src/services/etymologyUtils.ts
+++ src/services/etymologyUtils.ts
@@ -3,7 +3,7 @@ import type { EtymologyFeature } from "../model/EtymologyResponse";
 
 export function getEtymologies(feature: EtymologyFeature): Etymology[] | undefined {
     if (Array.isArray(feature.properties?.etymologies)) {
-        return feature.properties.etymologies;
+        return feature.properties?.etymologies;
     } else if (feature.properties?.etymologies) {
         feature.properties.etymologies = JSON.parse(feature.properties.etymologies) as Etymology[];
         return feature.properties.etymologies;
