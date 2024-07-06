3eec778437a2c5ee2a9310ea5026e566b3055538
Daniele Santini <danysan95@gmail.com>
2024-07-06 22:34:35 +0200

    Improve metadata generation

--- front-end/app/[lang]/page.tsx
+++ front-end/app/[lang]/page.tsx
@@ -1,8 +1,7 @@
 import { OwmfMapIfSupported } from "@/src/components/map/OwmfMapIfSupported";
 import { LANGUAGES } from "@/src/i18n/common";
-import { loadServerI18n } from "@/src/i18n/server";
+import { generateOwmfMetadata } from "@/src/metadata";
 import type { Metadata } from "next";
-import { Languages } from "next/dist/lib/metadata/types/alternative-urls-types";
 import styles from "./page.module.css";
 
 // https://nextjs.org/docs/app/building-your-application/routing/internationalization#static-generation
@@ -15,33 +14,7 @@ interface LanguageHomeProps {
 }
 
 export async function generateMetadata({ params: { lang } }: LanguageHomeProps): Promise<Metadata> {
-  const { t, i18nInstance } = await loadServerI18n(lang);
-  return {
-    title: t("title"),
-    applicationName: t("title"),
-    description: t("description"),
-    alternates: {
-      canonical: process.env.owmf_home_url,
-      languages: i18nInstance.languages?.reduce((acc: Languages<string>, lang) => {
-        acc[lang as keyof Languages<string>] = `${process.env.owmf_home_url}?lang=${lang}`;
-        return acc;
-      }, {}),
-    },
-    openGraph: {
-      siteName: t("title"),
-      title: t("title"),
-      description: t("description"),
-      type: 'website',
-      locale: i18nInstance.language,
-    },
-    icons: {
-      icon: ["favicon.svg", "favicon.ico"],
-      apple: "apple-touch-icon.png"
-    },
-    authors: [{ name: "Daniele Santini", url: "https://www.dsantini.it" }],
-    keywords: process.env.owmf_keywords,
-    robots: "index, follow",
-  }
+  return generateOwmfMetadata(lang);
 }
 
 export default function LanguageHome() {
--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,10 +1,16 @@
 import { OwmfMapIfSupported } from "@/src/components/map/OwmfMapIfSupported";
+import { generateOwmfMetadata } from "@/src/metadata";
+import { Metadata } from "next";
 import styles from "./page.module.css";
 
+export async function generateMetadata(): Promise<Metadata> {
+  return generateOwmfMetadata();
+}
+
 export default function RootHome() {
-    return (
-        <main className={styles.main}>
-            <OwmfMapIfSupported />
-        </main>
-    );
+  return (
+    <main className={styles.main}>
+      <OwmfMapIfSupported />
+    </main>
+  );
 }
