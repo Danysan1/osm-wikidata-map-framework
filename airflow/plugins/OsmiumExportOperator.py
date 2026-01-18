ca6cdc0eb410e1e5625f86c78ea919ac940d857d
Daniele Santini <danysan95@gmail.com>
2026-01-18 09:01:20 +0100

    Fix feature popup buttons + update osmium

--- airflow/plugins/OsmiumExportOperator.py
+++ airflow/plugins/OsmiumExportOperator.py
@@ -1,5 +1,6 @@
 from OsmDockerOperator import OsmDockerOperator
 
+
 class OsmiumExportOperator(OsmDockerOperator):
     """
     ## Operator for `osmium export`
@@ -12,13 +13,13 @@ class OsmiumExportOperator(OsmDockerOperator):
     * [index/cache documentation](https://docs.osmcode.org/osmium/latest/osmium-index-types.html)
     * Possible Docker images:
     * - [beyanora/osmtools](https://hub.docker.com/r/beyanora/osmtools) => stuck to 2021 / 1.13.1, requires "osmium" at the beginning of the command
-    * - [iboates/osmium](https://hub.docker.com/r/iboates/osmium) => updated (as of 2024 / 1.16.0)
+    * - [iboates/osmium](https://hub.docker.com/r/iboates/osmium) => updated (1.18.0 as of 2026)
     """
-    def __init__(self, source_path:str, dest_path:str, cache_path:str = None, config_path:str = None, **kwargs) -> None:
+    def __init__(self, source_path:str, dest_path:str, cache_path:str|None = None, config_path:str|None = None, **kwargs) -> None:
         cache_str = f"--index-type='sparse_file_array,{cache_path}'" if cache_path != None else ""
         config_str = f"--config='{config_path}'" if config_path != None else ""
         super().__init__(
-            image='iboates/osmium:1.16.0',
+            image='iboates/osmium:1.18.0',
             command = f"export --verbose --progress --overwrite -o '{dest_path}' -f 'pg' {config_str} --add-unique-id='counter' {cache_str} --show-errors '{source_path}'",
             **kwargs
         )
--- airflow/plugins/OsmiumTagsFilterOperator.py
+++ airflow/plugins/OsmiumTagsFilterOperator.py
@@ -1,5 +1,5 @@
 from OsmDockerOperator import OsmDockerOperator
-from typing import Union
+
 
 class OsmiumTagsFilterOperator(OsmDockerOperator):
     """
@@ -13,15 +13,15 @@ class OsmiumTagsFilterOperator(OsmDockerOperator):
     * [beyanora/osmtools Docker image details](https://hub.docker.com/r/beyanora/osmtools/tags)
     * Possible Docker images:
     * - [beyanora/osmtools](https://hub.docker.com/r/beyanora/osmtools/tags) => stuck to 2021 / 1.13.1, requires "osmium" at the beginning of the command
-    * - [iboates/osmium](https://hub.docker.com/r/iboates/osmium/tags) => updated (as of 2024 / 1.16.0)
+    * - [iboates/osmium](https://hub.docker.com/r/iboates/osmium/tags) => updated (1.18.0 as of 2026)
     """
 
-    def __init__(self, source_path:str, dest_path:str, tags:Union[list,str], invert_match:bool = False, remove_tags:bool = False, **kwargs) -> None:
+    def __init__(self, source_path:str, dest_path:str, tags:list|str, invert_match:bool = False, remove_tags:bool = False, **kwargs) -> None:
         invert_match_str = "--invert-match" if invert_match else ""
         remove_tags_str = "--remove-tags" if remove_tags else ""
         quoted_tags = tags if isinstance(tags, str) else ' '.join(map(lambda tag: f"'{tag}'", tags))
         super().__init__(
-            image='iboates/osmium:1.16.0',
+            image='iboates/osmium:1.18.0',
             command = f"tags-filter --verbose --progress --input-format=pbf --output-format=pbf {invert_match_str} {remove_tags_str} --output='{dest_path}' --overwrite '{source_path}' {quoted_tags}",
             **kwargs
         )
